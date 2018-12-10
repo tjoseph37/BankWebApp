5a40e23e9ab6302b521e14d32efb9266b220f23b
tjoseph37 <tj14@my.fsu.edu>
2018-12-09 20:35:20 -0500

    Add files via upload

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //Notes
+        //? is URL is seperator and query string follows, & seperates query strings
+        //Controller method generally returns IActionResult result 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View(); //Returns view object that generates html response
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+            //modifying url input 
+        public IActionResult About()
+        { 
+            return View();
+        }
+
+       
+    }
+}
